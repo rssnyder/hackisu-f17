adc3a47dc4aac6f59c018a06432dc74355ce4779
mr-scneider <rssnyder@iastate.edu>
2017-10-20 21:57:48 -0500

    Created python function to get search results

---
+++ prescriptionRefill/getItem.py
@@ -0,0 +1,27 @@
+import urllib
+import json
+
+def getItem(str):
+    # item = str(search)
+    url = "https://www.hy-vee.com/grocery/calls/SearchList.aspx?search=" + str + "&dep=0&depgroup=0&cat=0&subcat=0&brands=&diets=&sizes=&onsale=0&fuelsaver=0&coupon=0&whatIBuy=0&startIndex=1&ReturnAmount=120&sortID=5&init=true&squID=63339564&lockerEligibleFilter=false&type=filter&sreID=0"
+    f = urllib.urlopen(url)
+    # Get the raw html
+    html = f.read()
+
+    # Find the beginning and the end of the json string
+    startOfJSON = html.find("dataLayer.push(") + 15
+    endOfJSON = html.find("productListGTM-1") - 12
+
+    # Clip the html down to just the json string and get into dictionary
+    raw_json = html[startOfJSON:endOfJSON] + '}';
+    # raw_json.rstrip("\r\n")
+    raw_json = raw_json.replace('\n', ' ').replace('\r', '').replace('\'', '\"')
+    parsed_json = json.loads(raw_json)
+
+    # Make the dict of items
+    items = parsed_json["ecommerce"]["impressions"]
+
+    print items[0]
+
+
+getItem("corn")
---
+++ prescriptionRefill/refill.py
@@ -0,0 +1,5 @@
+from splinter import Browser
+browser = Browser()
+
+# Go to refill site
+broswer.visit('https://www.hy-vee.com/my-pharmacy/refill')
