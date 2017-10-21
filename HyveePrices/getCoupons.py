c51fa09970f28ed777a45cc69f3081545279b4e5
mr-scneider <rssnyder@iastate.edu>
2017-10-21 15:17:26 -0500

    Rewrote getcoupons without bs

--- HyveePrices/getCoupons.py
+++ HyveePrices/getCoupons.py
@@ -1,43 +1,50 @@
 import urllib2
-from bs4 import BeautifulSoup
 
-def getCoupons():
-    # Pretend to be firefox and get the webpage
-    url = "https://www.couponmom.com/best-coupon-deals-hyvee"
-    headers = { 'User-Agent' : 'Mozilla/5.0' }
-    req = urllib2.Request(url, None, headers)
-    html = urllib2.urlopen(req).read()
+# Pretend to be firefox and get the webpage
+url = "https://www.couponmom.com/best-coupon-deals-hyvee"
+headers = { 'User-Agent' : 'Mozilla/5.0' }
+req = urllib2.Request(url, None, headers)
+html = urllib2.urlopen(req).read()
 
-    # Use BeautifulSoup
-    soup = BeautifulSoup(html, 'html.parser')
+# Use BeautifulSoup
+#soup = BeautifulSoup(html, 'html.parser')
 
-    #Get all the deals
-    deals = BeautifulSoup(str(soup.find_all('ol', 'hot_deal_list', 'href')), 'html.parser')
+# We cant use beautifulsoup :( Get all the deals
+#deals = BeautifulSoup(str(soup.find_all('ol', 'hot_deal_list', 'href')), 'html.parser')
 
-    # Storage for deals
-    allCoupons = []
-    count = 0
+#get the start of the coupons
+start = html.find('<ol class=\"hot_deal_list\">') + 27
+html = html[start:]
 
-    # Get the item
-    # item = deals.find_all('a')[0]
-    for deal in deals.find_all('a'):
-        item = str(deal)
+# list for coupons
+allCoupons = []
+count = 0
 
-        # Get the deal price
-        startPrice = item.find("<span class=\"\">") + 15
-        endPrice = item.find(":")
-        itemPrice = item[startPrice:endPrice]
+theEnd = 1;
+while bool(theEnd):
+    # Get coupon price
+    startPrice = html.find('<span class=\"\">') + 15
+    endPrice = html.find(':')
+    itemPrice = html[startPrice:endPrice]
 
-        # Get the deal name
-        startDesc = item.find("</span>\\n") + 9
-        endDesc = item.find("</a>")
-        itemDesc = item[startDesc:endDesc]
+    # Get coupon desc
+    startDesc = html.find('</span>') + 8
+    endDesc = html.find('</a>')
+    itemDesc = html[startDesc:endDesc]
+    itemDesc = itemDesc.replace("ct", "count")
+    itemDesc = itemDesc.replace("oz", "ounces")
 
-        # Correct the short words for better alexa speak
-        itemDesc = itemDesc.replace("ct", "count")
-        itemDesc = itemDesc.replace("oz", "ounces")
+    # cut the html down to get next item
+    nextItem = html.find('</li>') + 6
+    html = html[nextItem:]
 
-        allCoupons.insert(count, itemDesc + 'are on sale for ' + itemPrice)
-        count = count + 1
+    #build text
+    allCoupons.insert(count, itemDesc + 'are on sale for ' + itemPrice)
+    count = count + 1
 
-    # In the end we have list allCoupons that has the whole deals as strings
+    #check for end of coupons
+    end = html.find('</ol>')
+    if end < 10:
+        theEnd = 0
+
+print allCoupons
