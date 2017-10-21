7743aefd3113aa7ed751d4e7e1e765fe65f572e2
Stephanie Engelhardt <sae1@iastate.edu>
2017-10-20 22:52:10 -0500

    stuff from the advisor

--- prescriptionRefill/refill.py
+++ prescriptionRefill/refill.py
@@ -1,5 +1,9 @@
-from splinter import Browser
-browser = Browser()
-
-# Go to refill site
-broswer.visit('https://www.hy-vee.com/my-pharmacy/refill')
+import requests
+url = "http://somewebsite.com/prescriptions"
+payload = "{'someContent':'SomeValue'}"
+headers = {
+    'accept': "application/json",
+    'content-type': "application/json; charset=UTF-8"
+    }
+response = requests.request("POST", url, data=payload, headers=headers)
+print(response.text)
