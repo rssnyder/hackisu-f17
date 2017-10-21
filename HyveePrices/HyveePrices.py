a5d449b746c65d954b5e395d924cd42478b26770
jaggarwal96 <jaggarwal@wisc.edu>
2017-10-20 23:31:17 -0500

    on Start function implemented

--- HyveePrices/HyveePrices.py
+++ HyveePrices/HyveePrices.py
@@ -50,13 +50,13 @@ def get_welcome_response():
 
     session_attributes = {}
     card_title = "Welcome"
-    speech_output = "Welcome to the Alexa Skills Kit sample. " \
-                    "Please tell me your favorite color by saying, " \
-                    "my favorite color is red"
+    speech_output = "Welcome to Hyvee Prices. " \
+                    "Please tell me what item's price you'd like to know by saying, " \
+                    "what is the price of steak"
     # If the user either does not reply to the welcome message or says something
     # that is not understood, they will be prompted again with this text.
-    reprompt_text = "Please tell me your favorite color by saying, " \
-                    "my favorite color is red."
+    reprompt_text = "Please tell me what item's price you'd like to know by saying, " \
+                    "what is the price of steak"
     should_end_session = False
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
