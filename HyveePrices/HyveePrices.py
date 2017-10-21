3f3d0b3cf968d5ea3e890cd0c3af6fbccdd5ee10
Joel Schwartz <joel2@iastate.edu>
2017-10-21 17:26:02 -0500

    added news

--- HyveePrices/HyveePrices.py
+++ HyveePrices/HyveePrices.py
@@ -124,6 +124,71 @@ def get_coupons():
 
     return allCoupons
 
+def get_news():
+    # Pretend to be firefox and get the webpage
+    url = "https://www.hy-vee.com/company/press-room/"
+
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    req = urllib2.Request(url, None, headers)
+    html = urllib2.urlopen(req).read()
+
+    # Test and see if we can get the news
+    uptime = html.find('<html><head><meta http-equiv=\"Pragma\" content=\"no-cache\"/>')
+    if uptime >= 0:
+        url = "https://www.hy-vee.com/company/press-room/default.aspx?viewall=1"
+        html = urllib2.urlopen(req).read()
+        uptime2 = html.find('equiv=\"Pragma\" content=\"no-cache\"/>')
+        if uptime2 >= 0:
+            var = 1
+    # This means we are not able to get news right now
+
+    # Cut the html down to the main news articles
+    start = html.find('ctl00_cph_main_content_ArticleList_gvRecentArticles')
+    html = html[start:]
+
+    # List for news articles
+    allNews = []
+    count = 0
+    end = 1000
+
+    while end > 100:
+        # Find the title of the news item
+        start = html.find('<a')
+        html = html[start:]
+        start = html.find('>') + 1
+        # Here we have the start of the title
+        html = html[start:]
+        html = html.lstrip()
+        # Here we mark the end of the title
+        end = html.find('</a>')
+        item = html[:end]
+        # Find the beginning of the description
+        start = html.find('<br/>') + 6
+        html = html[start:]
+        html = html.lstrip()
+        # Find the end of the description
+        end = html.find('<a')
+        desc = html[:end]
+        desc = desc.replace('\n', ' ')
+        # Create article and store
+        item = item + ': ' + desc
+        allNews.insert(count, item)
+        count = count + 1
+
+        # Chop off some extra
+        stop = html.find('</a>')
+        html = html[stop:]
+
+        end = html.find('</table>')
+        print
+        html
+        print
+        end
+
+    return allNews
+
+        # allNews is the list of news snippits
+
 
 # --------------- Functions that control the skill's behavior ------------------
 
@@ -250,6 +315,22 @@ def set_coupons(intent, session):
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
 
+def get_the_news(intent, session):
+    card_title = intent['name']
+    session_attributes = {}
+    should_end_session = False
+
+    news = get_news()
+
+    rand = random.randint(0, len(news)-1)
+    speech_output = news[rand]
+
+    reprompt_text = "What would you like to do?"
+
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+
 
 # --------------- Events ------------------
 
@@ -287,6 +368,8 @@ def on_intent(intent_request, session):
         return set_coupons(intent, session)
     elif intent_name == "GetSmile" :
         return get_smile(intent, session)
+    elif intent_name == "HyveeNews" :
+        return get_the_news(intent, session)
     elif intent_name == "AMAZON.HelpIntent":
         return helper_method_get_instructions()
     elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
