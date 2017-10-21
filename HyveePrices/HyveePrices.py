50d80bf031f616404c8a13ec140abb7e55b7077c
jaggarwal96 <jaggarwal@wisc.edu>
2017-10-21 01:20:53 -0500

    more than one requests

--- HyveePrices/HyveePrices.py
+++ HyveePrices/HyveePrices.py
@@ -9,6 +9,8 @@ http://amzn.to/1LGWsLG
 
 from __future__ import print_function
 
+firstRun = True
+
 
 # --------------- Helpers that build all of the responses ----------------------
 
@@ -58,6 +60,7 @@ def get_welcome_response():
     reprompt_text = "Please tell me what item's price you'd like to know by saying, " \
                     "what is the price of steak"
     should_end_session = False
+    firstRun =  False
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
 
@@ -68,8 +71,10 @@ def helper_method_get_instructions():
     card_title = "Help"
     speech_output = "Please tell me what item's price you'd like to know by saying, " \
                     "what is the price of steak"
+    # If the user either does not reply to the welcome message or says something
+    # that is not understood, they will be prompted again with this text.
+    reprompt_text = "What would you like price of? "
 
-    reprompt_text = "What would you like me to do?"
     should_end_session = False
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text,should_end_session))
@@ -156,6 +161,20 @@ def on_launch(launch_request, session):
     # Dispatch to your skill's launch
     return get_welcome_response()
 
+def query_after_first(intent, session):
+
+    card_title = intent['name']
+    session_attributes = {}
+    speech_output = "Is " + intent['slots']['food']['value'] + " the item you want? Reply with yes or no."
+
+    should_end_session = False
+
+    reprompt_text = "I am sorry. Is " + intent['slots']['food']['value'] + " the item you want? Reply with yes or no"
+
+    should_end_session = True
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
 
 def on_intent(intent_request, session):
     """ Called when the user specifies an intent for this skill """
@@ -167,10 +186,12 @@ def on_intent(intent_request, session):
     intent_name = intent_request['intent']['name']
 
     # Dispatch to your skill's intent handlers
-    if intent_name == "MyColorIsIntent":
+    if intent_name == "FoodIntent":
         return set_food_in_session(intent, session)
     elif intent_name == "WhatsMyColorIntent":
         return get_color_from_session(intent, session)
+    elif intent_name == "QueryAfterFirst" and firstRun == False:
+        return query_after_first(intent, session)
     elif intent_name == "AMAZON.HelpIntent":
         return helper_method_get_instructions()
     elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
